e0c246a89ca797d8cbfbcb6679c78b876140b110
cathy-ngo <cathy.x86@gmail.com>
2022-04-27 17:42:32 +0800

    Settings Button + Light Mode
    Still a work in progress - need to fix some light mode issues e.g. update colour for system modal, create goldBorder() for input box hover.

--- static/scripts/scripts.js
+++ static/scripts/scripts.js
@@ -53,3 +53,69 @@ function displayGuess(guess) {
     $('#results').append(card);
 }
 
+function openSettings() {
+    // Get the modal
+    var modal = document.getElementById("settingsModal");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function lightMode() {
+    var element = document.body;
+    var lightSwitch = document.getElementById("light-switch");
+    var inputBox = document.getElementsByClassName("inputbox")[0];
+    var userInput = document.getElementById("userinput");
+    var submitButton = document.getElementById("submit");
+
+    
+    element.classList.toggle("light-mode");
+
+    if(lightSwitch.checked) {
+        // Input Box Styling
+        inputBox.style.backgroundColor="#F4F4F3";
+        inputBox.style.color ="#033039";
+
+        // User Input Box Styling
+        userInput.style.backgroundColor="#F4F4F3";
+        userInput.style.color="#214249";
+        userInput.style.borderColor="#214249";
+
+        // Submit Button Styling
+        submitButton.style.color="#F4F4F3";
+        submitButton.style.borderColor="#214249";
+        submitButton.style.backgroundColor="#214249";
+
+        // Settings
+
+    }
+
+    else {
+        // Input Box Styling
+        inputBox.style.backgroundColor="#052329";
+        inputBox.style.color="#F4F4F3";
+
+        // User Input Box Styling
+        userInput.style.backgroundColor="#052329";
+        userInput.style.color="#F4F4F3";
+        userInput.style.borderColor="#F4F4F3";
+        
+        // Submit Button Styling
+        submitButton.style.color="#052329";
+        submitButton.style.borderColor="#F4F4F3";
+        submitButton.style.backgroundColor="#F4F4F3";
+    }
+}
