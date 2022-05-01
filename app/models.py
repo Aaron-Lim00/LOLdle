b8ec5d773e2171c89e4d66725d74c377891afd19
SleepyPanda00 <22513032@student.uwa.edu.au>
2022-05-01 17:27:10 +0800

    Updated login functionality
    Still need to fix password hashing and remember login features via flask

--- app/models.py
+++ app/models.py
@@ -1,5 +1,6 @@
 from app import db
 
+
 # define a champion class that inherits from db.Model
 class Champion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -11,3 +12,18 @@ class Champion(db.Model):
     # for debugging
     def __repr__(self):
         return '<Champion {}>'.format(self.name)
+
+#defines the user database
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+
+
+# The following are required for login management
+
