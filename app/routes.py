da18640987832c70d09b141f8c63f824dd872805
cathy-ngo <cathy.x86@gmail.com>
2022-05-01 21:02:52 +0800

    Flask handles POST requests from index form
    Displays champion name in the database - WIP

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from app import app
+from app import app, db
+from app.models import Champion
 from flask import render_template, url_for, request, jsonify
 
 # Route for the home page. The majority of the page will be displayed here. 
@@ -10,10 +11,15 @@ def index():
 # Process form 
 @app.route('/process', methods=['POST'])
 def process():
-    # Get data from request object
-    champion = request.form.get('input', False)
+    # Get data from request object - champion name
+    champion = request.form
+    champ2 = champion.to_dict()['champion']
 
-    # Do something
-    newChamp = "hello please work"
+    # Find champion data in champion database
+    champ = Champion.query.filter_by(name=champ2).first()
+    champ_name = champ.name
+    champ_role = champ.role
+    champ_year = champ.year 
+    champ_skins = champ.skins
 
-    return jsonify({'champion' : newChamp})
+    return jsonify({'champion' : champ_name, 'role' : champ_role, 'year' : champ_year, 'skins' : champ_skins}) 
