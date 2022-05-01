b3be4e17db08630fc507c05baa5cec679df8f964
SleepyPanda00 <22513032@student.uwa.edu.au>
2022-05-01 17:28:33 +0800

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,5 @@
 from app import app
 from flask import render_template, url_for
-from flask_login import current_user, login_user
-from app.models import User
 
 # Route for the home page. The majority of the page will be displayed here. 
 @app.route('/')
