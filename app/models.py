496c5c9b2638b48d2d07cbb5c3c640d1ab9dd6ec
Aaron Lim <65753842+Aaron-Lim00@users.noreply.github.com>
2022-05-18 12:02:17 +0800

    Login db working
    Still needs login verification functionality

--- app/models.py
+++ app/models.py
@@ -1,4 +1,7 @@
 from app import db
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+# from app import login
 
 # define a champion class that inherits from db.Model
 class Champion(db.Model):
@@ -13,7 +16,6 @@ class Champion(db.Model):
         return '<Champion {}>'.format(self.name)
 
 class User(db.Model):
-    __bind_key__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -21,3 +23,17 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+
+#     def set_password(self, password):
+#         self.password_hash = generate_password_hash(password)
+
+#     def check_password(self, password):
+#         return check_password_hash(self.password_hash, password)
+
+# class User(UserMixin, db.Model):
+
+#     @login.user_loader
+#     def load_user(id):
+#         return User.query.get(int(id))
+
+# Still need to write class linking answers to users 
--- config.py
+++ config.py
@@ -9,27 +9,3 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    SQLALCHEMY_BINDS = {
-        'user': 'sqlite:///' + os.path.join(basedir, 'user.db')
-    }
-
-    # # may or may not be required
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-    #     'sqlite:///' + os.path.join(basedir, 'login.db')
-    # SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-class DbConfig(object):
-    database = "database_name"
-    user = "db_user"
-    password = "password"
-    host = "localhost"
-    unix_socket = "/var/run/mysqld/mysqld.sock"
-    SQLALCHEMY_DATABASE_URI = 'mysql://'+ user + ':' + password + '@' + host + '/' + database +'?unix_socket='+ unix_socket
-    SQLALCHEMY_BINDS = {
-        'user': 'mysql://user:pass@localhost/database2'
-    }
-        
-        
-
-
