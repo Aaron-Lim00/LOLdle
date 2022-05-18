c7678ceb186c5b077dec9a6b297b3624d580adde
Aaron Lim <65753842+Aaron-Lim00@users.noreply.github.com>
2022-05-18 17:12:47 +0800

    Login html page working

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app, db
 from app.models import Champion
-from flask import render_template, url_for, request, jsonify
+from flask import render_template, url_for, request, jsonify, flash, redirect
+from app.forms import LoginForm
 import datetime
 import random
 
@@ -76,3 +77,12 @@ def process():
         'skins' : skins_feedback,
         'skinvalue' : champ.skins
         }) 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect('/index')
+    return render_template('login.html', title='Sign In', form=form)
