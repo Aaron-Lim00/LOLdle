5e1188cb4529b2802d4a83fa33be4f1a0a76db8b
Aaron Lim <65753842+Aaron-Lim00@users.noreply.github.com>
2022-05-19 11:18:25 +0800

    db query working

--- app/routes.py
+++ app/routes.py
@@ -84,5 +84,5 @@ def login():
     if form.validate_on_submit():
         flash('Login requested for user {}, remember_me={}'.format(
             form.username.data, form.remember_me.data))
-        return redirect('/index')
+        return redirect(url_for('index'))
     return render_template('login.html', title='Sign In', form=form)
