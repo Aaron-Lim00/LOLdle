77eb0075fa4724aa57c4eeae2f9a1990b7ab80c1
Aaron Lim <65753842+Aaron-Lim00@users.noreply.github.com>
2022-05-19 16:35:01 +0800

    Working login

--- app/models.py
+++ app/models.py
@@ -1,8 +1,10 @@
-from app import db
+from app import db, login
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
-# from app import login
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 # define a champion class that inherits from db.Model
 class Champion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -15,7 +17,22 @@ class Champion(db.Model):
     def __repr__(self):
         return '<Champion {}>'.format(self.name)
 
-class User(db.Model):
+# class User(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.String(64), index=True, unique=True)
+#     email = db.Column(db.String(120), index=True, unique=True)
+#     password_hash = db.Column(db.String(128))
+
+#     def __repr__(self):
+#         return '<User {}>'.format(self.username)
+
+#     def set_password(self, password):
+#         self.password_hash = generate_password_hash(password)
+
+#     def check_password(self, password):
+#         return check_password_hash(self.password_hash, password)
+
+class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -24,16 +41,14 @@ class User(db.Model):
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-#     def set_password(self, password):
-#         self.password_hash = generate_password_hash(password)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
-#     def check_password(self, password):
-#         return check_password_hash(self.password_hash, password)
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
-# class User(UserMixin, db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
-#     @login.user_loader
-#     def load_user(id):
-#         return User.query.get(int(id))
-
-# Still need to write class linking answers to users 
+# Still need to write class linking answers to users
--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from app import app, db
-from app.models import Champion
+from flask_login import current_user, login_user, logout_user
+from app.models import Champion, User
 from flask import render_template, url_for, request, jsonify, flash, redirect
-from app.forms import LoginForm
+from app.forms import LoginForm, RegistrationForm
+from werkzeug.urls import url_parse
 import datetime
 import random
 
@@ -18,6 +20,7 @@ def home():
     return render_template("index.html", champions=champions)
 
 # Route for the home page. The majority of the page will be displayed here. 
+@app.route('/')
 @app.route('/index')
 def index():
     return render_template('index.html')
@@ -78,11 +81,54 @@ def process():
         'skinvalue' : champ.skins
         }) 
 
+# @app.route('/login', methods=['GET', 'POST'])
+# def login():
+#     form = LoginForm()
+#     if form.validate_on_submit():
+#         flash('Login requested for user {}, remember_me={}'.format(
+#             form.username.data, form.remember_me.data))
+#         return redirect(url_for('index'))
+#     return render_template('login.html', title='Sign In', form=form)
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
+    if current_user.is_authenticated:
         return redirect(url_for('index'))
+    form = LoginForm()
+    # if form.validate_on_submit():
+    #     user = User.query.filter_by(username=form.username.data).first()
+    #     if user is None or not user.check_password(form.password.data):
+    #         flash('Invalid username or password')
+    #         return redirect(url_for('login'))
+    #     login_user(user, remember=form.remember_me.data)
+    #     return redirect(url_for('index'))
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, email=form.email.data)
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('login'))
+    return render_template('register.html', title='Register', form=form)
