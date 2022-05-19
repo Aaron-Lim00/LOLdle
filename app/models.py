103e4c52977c72108189b5d5a78c7cabfa16b02e
Aaron Lim <65753842+Aaron-Lim00@users.noreply.github.com>
2022-05-19 17:28:09 +0800

    Update models.py

--- app/models.py
+++ app/models.py
@@ -13,24 +13,9 @@ class Champion(db.Model):
     year = db.Column(db.Integer)
     skins = db.Column(db.Integer)
 
-    # for debugging
+    
     def __repr__(self):
         return '<Champion {}>'.format(self.name)
-
-# class User(db.Model):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(64), index=True, unique=True)
-#     email = db.Column(db.String(120), index=True, unique=True)
-#     password_hash = db.Column(db.String(128))
-
-#     def __repr__(self):
-#         return '<User {}>'.format(self.username)
-
-#     def set_password(self, password):
-#         self.password_hash = generate_password_hash(password)
-
-#     def check_password(self, password):
-#         return check_password_hash(self.password_hash, password)
 
 class User(UserMixin,db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -51,4 +36,4 @@ class User(UserMixin,db.Model):
 def load_user(id):
     return User.query.get(int(id))
 
-# Still need to write class linking answers to users
+
