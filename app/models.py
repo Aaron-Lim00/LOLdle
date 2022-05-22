adbaddbfa407db860ad85c7de71ef8394016d795
Glen Nguyen <glendle.nguyen@gmail.com>
2022-05-22 23:05:37 +0800

    Revert "Login working2"

--- app/models.py
+++ app/models.py
@@ -1,11 +1,5 @@
-# from typing_extensions import Self
-from app import db, login
-from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import UserMixin
+from app import db
 
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
 # define a champion class that inherits from db.Model
 class Champion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -14,43 +8,7 @@ class Champion(db.Model):
     year = db.Column(db.Integer)
     skins = db.Column(db.Integer)
 
-    
+    # for debugging
     def __repr__(self):
         return '<Champion {}>'.format(self.name)
-
-class User(UserMixin,db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    scores = db.relationship('Score', backref='summoner', lazy='select', uselist=False) #Link User and Score Database
-    # Unsure what backref=taker means
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-class Score(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # Datbase fields to be updated
-    onlineGamesPlayed = db.Column(db.Integer)
-    onlineGamesWon = db.Column(db.Integer)
-    onlineAverageGuesses = db.Column(db.Integer)
-    # Links score database to user db 
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    
-    def __repr__(self):
-        return '<Score {}>'.format(self.onlineGamesWon) 
-        # The above line may need to be changed 
-
-
-@login.user_loader
-def load_user(id):
-    return User.query.get(int(id))
-
 
--- app/static/scripts/scripts.js
+++ app/static/scripts/scripts.js
@@ -121,9 +121,10 @@ function clearStorage() {
 function share() {
     let winpercentage = (localStorage.gamesWon / localStorage.gamesPlayed).toFixed(2);
     let averageguesses = (localStorage.totalGuesses/localStorage.gamesPlayed).toFixed(2);
-    let gamesplayed = localStorage.gamesPlayed; 
+    let gamesplayed = localStorage.gamesPlayed;
     let gameswon = localStorage.gamesWon;
     let guesses = count;
+
     let result = ``;
     if(victory) {
         result += `Victory!\n`;
