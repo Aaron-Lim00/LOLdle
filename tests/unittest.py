e6cd0470be585cc4144fb2356e7a4274f1ace3c2
Alucaro <1793595+sean-alucaro@users.noreply.github.com>
2022-05-22 07:04:01 -0700

    basic unit test

--- tests/unittest.py
+++ tests/unittest.py
@@ -1,28 +1,29 @@
 import unittest, os
 from app import app, db
-from app.models import Champion
+from app.models import User
 
-class ChampionModelCase(unittest.TestCase):
+class UserModelCase(unittest.TestCase):
 
     def setUp(self):
-        basedir = os.path.abspath(os.path.dirname(__file__))
-        app.config['SQLALCHEMY_DATABASE_URI']=\
-            'sqlite:///'+os.path.join(basedir,'test.db')
-        # Creates virtual test env
-        self.app = app.test_client()
-        db.create_all()
-        champ = Champion('9999', 'champname', 'champrole', '2222', '100')
+            basedir = os.path.abspath(os.path.dirname(__file__))
+            app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'test.db')
+            self.app = app.test_client()
+            db.create_all()
+            user1 = User(id=0000000, username='user1', email='user1@mail.com' )
+            user2 = User(id=9999999, username='user2', email='user2@mail.com' )
+            db.session.add(user1)
+            db.session.add(user2)
+            db.session.commit()
 
     def tearDown(self):
         db.session.remove()
         db.drop_all()
-    
-    def test_inserting_champion(self):
-        assert champ.id == '9999'
-        assert champ.name == 'champname'
-        assert champ.role == 'champrole'
-        assert champ.year == '2222'
-        assert champ.skins == '100'
 
-    if __name__ == '__main__':
-        unittest.main()
+    def test_password_hash(self):
+        temp = User.query.get('0000000')
+        temp.set_password('qwerty')
+        self.assertFalse(temp.check_password('abdef'))
+        self.assertTrue(temp.check_password('qwerty'))
+
+if __name__ == '__main__':
+    unittest.main()
