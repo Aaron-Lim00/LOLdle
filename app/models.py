3a196d142258aeb24dd7182e9af43a2278553fbf
Aaron Lim <65753842+Aaron-Lim00@users.noreply.github.com>
2022-05-23 00:13:10 +0800

    Revert "Merge branch 'main' of https://github.com/Aaron-Lim00/LOLdle into conditional-stats"
    This reverts commit 55e7aa5e54eba5a274e4e0863c427267022b7823, reversing
    changes made to 613381a28dcbe7409dea6392a3e428a6ee99ceeb.

--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
-from app import db
+# from typing_extensions import Self
+from app import db, login
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 # define a champion class that inherits from db.Model
 class Champion(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -8,7 +14,37 @@ class Champion(db.Model):
     year = db.Column(db.Integer)
     skins = db.Column(db.Integer)
 
-    # for debugging
+    
     def __repr__(self):
         return '<Champion {}>'.format(self.name)
+
+class User(UserMixin,db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    scores = db.relationship('Score', backref='taker', lazy='dynamic') #Link User and Score Database
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Score(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    score = db.Column(db.Integer)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id')) #Links databases together
+    
+    def __repr__(self):
+        return '{}'.format(self.score)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
 
